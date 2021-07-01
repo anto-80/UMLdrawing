542f20b5ea03383aebbfc2a29127c65e8a33f3fe
Waweru <waweran@ad.vzwcorp.com>
2021-07-01 12:06:28 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+class Airport {
+    planes:Integer
+    runways:{Integer
+     assignrunways(){
+
+     }
+    }
+    terminals:{Integer
+     assignrunway(){
+
+     }}
+}
+
+,class Plane {
+    type:integer
+    capacity:Integer
+}
+
+class Passenger {
+    name:string
+    ticketNo:Float
+    section: string
+
+}
